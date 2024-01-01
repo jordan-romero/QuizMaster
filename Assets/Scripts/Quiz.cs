0013b83a3fa9a176711f9beaa7064d3907ede3d6
Jordan Romero <jordan.t.romero@gmail.com>
2024-01-01 15:32:39 -0600

    adds the ability to select answers and display the correct answer

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class Quiz : MonoBehaviour
     [SerializeField] QuestionSO question;
 
     [SerializeField] GameObject[] answerButtons;
+
+    [SerializeField] int correctAnswerIndex;
+
+    [SerializeField] Sprite defaultAnswerSprite;
+
+    [SerializeField] Sprite correctAnswerSprite;
 
 
     // Start is called before the first frame update
@@ -23,9 +30,23 @@ public class Quiz : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnAnswerSelected(int index)
     {
-        
+        Image buttonImage;
+
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
     }
 }
