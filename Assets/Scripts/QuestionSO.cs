ff562b6d1df444d46a88f6f8bc444651c060e840
Jordan Romero <jordan.t.romero@gmail.com>
2023-12-31 18:48:52 -0600

    adds answers

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -12,8 +12,18 @@ public class QuestionSO : ScriptableObject {
 
     [SerializeField] string[] answers = new string[4];
 
+    [SerializeField] int correctAnswerIndex;
+
     public string GetQuestion() {
         return question;
+    }
+
+    public string GetAnswer(int index) {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswerIndex;
     }
 }
 
